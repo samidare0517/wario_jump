6a80280da2c151108b47025f397c66e0230c9515
石橋 叶恵 <fko2247009@stu.o-hara.ac.jp>
2022-09-28 11:48:23 +0900

    車の挙動実装

--- car.cpp
+++ car.cpp
@@ -46,11 +46,37 @@ void Car::update()
 		m_waitFrame--;
 		return;
 	}
-	m_pos += m_vec;
+	m_pos += m_vec;		// まっすぐ進む
 }
 
 void Car::draw()
 {
 	DrawGraphF(m_pos.x, m_pos.y, m_handle, true);
-	DrawFormatString(0, 0, GetColor(225, 225, 225), "wait;%d", m_waitFrame);
+	DrawFormatString(0, 0, GetColor(225, 225, 225), "wait:%d", m_waitFrame);
+}
+
+// private
+
+// まっすぐ進む
+void Car::updateNormal()
+{
+	m_pos += m_vec;	
+}	
+
+// 一時停止フェイント
+void Car::updateStop()
+{
+
+}
+
+// ジャンプする
+void Car::updateJump()
+{
+
+}
+
+// 途中で引き返す(必ず成功)
+void Car::updateReturn()
+{
+
 }
